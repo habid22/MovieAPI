67a41a2b79a93cd8a970bbdeffdc96675528c102
habid22 <habid22@uwo.ca>
2024-05-29 12:43:09 -0400

    Added comments

--- api/database.js
+++ api/database.js
@@ -1,16 +1,19 @@
 const sqlite3 = require('sqlite3').verbose();
 
+// create an in-memory database
 const db = new sqlite3.Database(':memory:');
 
+// create a persistent database
 db.serialize(() => {
-  db.run(`CREATE TABLE movies (
+  // create a new table named 'movies'
+  db.run(`CREATE TABLE movies ( 
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT,
     year TEXT,
     plot TEXT,
     poster TEXT
   )`);
-
+  // create a new table named 'reviews'
   db.run(`CREATE TABLE reviews (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     movie_id INTEGER,
